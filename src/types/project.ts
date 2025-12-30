74b654085d47a1b6e50f2c695ea5d91a48e55d1a
Dong Duong <dongduong.dev@gmail.com>
2025-12-30 17:53:20 +0700

    initial backup before refactor

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  slug: string;
+  title: string;
+  location: string;
+  client: string;
+  time: string;
+  description: string;
+  services: string[];
+  gallery: string[];
+}
