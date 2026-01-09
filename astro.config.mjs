6306cabba63c479c02a97c026e06fddb80aebc13
TuanN <tuannguyenlq@outlook.com>
2026-01-09 14:40:43 +0700

    defaulf config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // NOTE: đổi 'dongduong' thành tên sub-folder bạn muốn
 export default defineConfig({
-  site : 'http://localhost:4321/dongduong/',   // URL đầy đủ dùng khi preview
-  base : '/dongduong/',                         // sub-folder
+                 
 });
